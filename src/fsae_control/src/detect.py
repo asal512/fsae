bae19550479604e19f58cb408051288e5da2c9aa
Aakaash Salvaji <asal512@aucklanduni.ac.nz>
2022-07-06 14:07:38 +1200

    complete setup of fsae environment for DQN-cartpole like problem

--- src/fsae_control/src/detect.py
+++ src/fsae_control/src/detect.py
@@ -3,13 +3,20 @@ from re import A
 from turtle import backward, distance
 import rospy
 import math
+from std_msgs.msg import Bool
 from geometry_msgs.msg import Point, Twist
 from cares_msgs.msg import ArucoMarkers
 
 REWARD_SCALAR = 10 #max reward value given when angle = 0 and distance = 0
 ANGLE_REWARD_DROPOFF = 6 #larger value = faster drop off of reward for increasing angle (frequency of cos wave)
-DISTANCE_REWARD_DROPOFF = 0.5 #larger value = faster drop off of reward for increasing distance (frequency of cos wave)
-ANGLE_TO_DISTANCE_REWARD_PRIORITY = 0.5 #ratio of total reward that is prioritized from angle reward (distance reward is 1 - ANGLE_TO_DISTANCE_REWARD_PRIORITY)
+DISTANCE_REWARD_DROPOFF = 4 #larger value = faster drop off of reward for increasing distance (frequency of cos wave)
+ANGLE_TO_DISTANCE_REWARD_PRIORITY = 0.15 #ratio of total reward that is prioritized from angle reward (distance reward is 1 - ANGLE_TO_DISTANCE_REWARD_PRIORITY)
+
+class ArucoPositionInfo():
+	def __init__(self):
+		self.x = 100.0
+		self.z = 100.0
+		self.id = 0
 
 class Detector():
 	def __init__(self):
@@ -24,25 +31,38 @@ class Detector():
 		
 		self.detector_sub = rospy.Subscriber("/camera/markers", ArucoMarkers, self.MarkerCallback)
 		self.teleop_sub = rospy.Subscriber("/cmd_vel_mux/input/teleop", Twist, self.TeleopCallback)
-		self.markers = ArucoMarkers()
-		self.input = Twist()
-		self.num_of_markers = 0
+		self.key_pressed_sub = rospy.Subscriber("/key_pressed", Bool, self.KeyPressedCallback)
 
+
+		self.markers = ArucoMarkers()
+		self.action = Twist()
+		self.num_of_markers = 0
+		self.key_pressed = False
+		self.prev_key_pressed = False
+		self.count = 0
+
+		#store previous iteration values
+		self.prev_marker_positions = []
+		self.prev_action = Twist()
+		self.prev_reward = 0
+
+		self.current_marker_positions = [ArucoPositionInfo(), ArucoPositionInfo(), ArucoPositionInfo(), ArucoPositionInfo(), ArucoPositionInfo(), ArucoPositionInfo()]
+		self.current_action = Twist()
+		self.current_reward = 0	
 
 		#reset data.txt file (clear all data)
-		file = open("/home/aakaash/fsae/src/fsae_control/data.txt","w")
-		file.close()
+		#file = open("/home/aakaash/fsae/src/fsae_control/data.txt","w")
+		#file.close()
 
-		# TurtleBot will stop if we don't keep telling it to move.  
-		# How often should we tell it to move?
-		r = rospy.Rate(5)
-		
+		r = rospy.Rate(20)
 		
 		self.OpenFile()
 
 		# As long as you haven't ctrl + c keeping doing...
 		while not rospy.is_shutdown():
-			self.AppendDataToFile()
+			self.GetCurrentData()
+			#self.AppendDataToFile()
+			print(self.CalculateReward())
 			r.sleep()
 
 
@@ -50,9 +70,6 @@ class Detector():
 		# Stop turtlebot
 		rospy.loginfo("Stop TurtleBot")
 		self.CloseFile()
-		# A default Twist has linear.x of 0 and angular.z of 0.  
-		# So it'll stop TurtleBot
-		#self.cmd_vel.publish(Twist())
 		# Sleep just makes sure TurtleBot receives the stop command
 		# prior to shutting
 		# down the script
@@ -62,11 +79,12 @@ class Detector():
 		self.markers = data
 		self.num_of_markers = len(self.markers.marker_ids)
 		self.ReorderMarkers()
-		#self.FindLowestPair()
-		#print("Reward = %.3f \n\n" % self.CalculateReward())
 
 	def TeleopCallback(self, data):
-		self.input = data
+		self.action = data
+
+	def KeyPressedCallback(self, data):
+		self.key_pressed = data.data
 		
 	def CalculateMidPoint(self, mk1, mk2):
 		midpoint = Point()
@@ -112,39 +130,63 @@ class Detector():
 			if (first_marker_id % 2 == 1):
 				if(self.markers.marker_ids[index + 1] == first_marker_id + 1):
 					self.first_marker_index = index
+					#print("ids = " + str(self.markers.marker_ids[index]) + " & " + str(self.markers.marker_ids[index+1]))
 					break
 
 	def CalculateReward(self):
 		self.FindLowestPair()
-
 		if(self.first_marker_index < 0):
-			return -10 * REWARD_SCALAR
-
+			return 0
+		
 		midpoint = self.CalculateMidPoint(self.markers.marker_poses[self.first_marker_index],self.markers.marker_poses[self.first_marker_index + 1])
 		angleToTarget = self.CalculateAngleTo(midpoint)
-		distanceToTarget = self.CalculateDistanceTo(midpoint)
+		#distanceToTarget = self.CalculateDistanceTo(midpoint)
+		angle_reward = REWARD_SCALAR*math.cos(angleToTarget * ANGLE_REWARD_DROPOFF)
 
-		angle_reward = REWARD_SCALAR* math.cos(angleToTarget * ANGLE_REWARD_DROPOFF)
+		if angle_reward < 0:
+			angle_reward = 0
+		# distance_reward = REWARD_SCALAR* math.cos(distanceToTarget * DISTANCE_REWARD_DROPOFF) dropoff = 1.5 (cos - gives negative values)
+		# distance_reward = REWARD_SCALAR* math.exp(distanceToTarget * DISTANCE_REWARD_DROPOFF) dropoff = -0.5 (exp - decent performance)
+		#distance_reward = REWARD_SCALAR*1.5 - (distanceToTarget * DISTANCE_REWARD_DROPOFF) #dropoff = 2 (linear)
+		
+		# dont let distance reward go negative	
+		#if distance_reward < 0:
+		#	distance_reward = 0	
+		
+		#return (ANGLE_TO_DISTANCE_REWARD_PRIORITY*angle_reward) + ((1 - ANGLE_TO_DISTANCE_REWARD_PRIORITY)*distance_reward)
+		return angle_reward
+		#print ("distance = " + str(distance_reward))
+		#print ("angle = " + str(angle_reward))
+		#print ("total = " + str((ANGLE_TO_DISTANCE_REWARD_PRIORITY*angle_reward) + ((1 - ANGLE_TO_DISTANCE_REWARD_PRIORITY)*distance_reward)) + "\n")
 
-		distance_reward = REWARD_SCALAR* math.cos(distanceToTarget * DISTANCE_REWARD_DROPOFF)
-
-		return (ANGLE_TO_DISTANCE_REWARD_PRIORITY*angle_reward) + ((1 - ANGLE_TO_DISTANCE_REWARD_PRIORITY)*distance_reward)
 
 	def AppendDataToFile(self):
-		#get marker positions
-		marker_positions = self.GetData()
-		data_line = ""
-		#write marker x,z values
-		for i in range(6):
-			data_line += "%.3f %.3f " % (marker_positions[i].x, marker_positions[i].z)
-		#write teleop cmd values
-		data_line += str(self.input.linear.x) + " " + str(self.input.angular.z) + " "
-		#write reward
-		data_line += str(self.CalculateReward())
+		#print only if teleop keys pressed
+		if(self.key_pressed and self.prev_key_pressed and self.count > 5):
+			data_line = ""
+			#write marker x,z values
+			for i in range(6):
+				data_line += "%d %.3f %.3f " % (self.prev_marker_positions[i].id, self.prev_marker_positions[i].x, self.prev_marker_positions[i].z)
+			#write teleop cmd values
+			data_line += str(self.prev_action.linear.x) + " " + str(self.prev_action.angular.z) + " "
+			#write reward
+			data_line += str(self.prev_reward) + "\n"
+			#print(str(self.current_reward) + "\n")
+			#data_line += str(self.current_reward - self.prev_reward) + "\n"
+			#print(str(self.current_reward - self.prev_reward) + "\n")
+			#write data to file	
+			#print(data_line + "\n")
+			self.AppendToFile(data_line)
+		elif(not self.key_pressed and self.prev_key_pressed):
+			self.CloseFile()
+		elif(self.key_pressed and not self.prev_key_pressed):
+			self.OpenFile()
+		self.count+=1
 
-		print(data_line)
-		#write data to file	
-		self.AppendToFile(data_line)
+		self.prev_action = self.current_action
+		self.prev_marker_positions = self.current_marker_positions
+		self.prev_reward = self.current_reward
+		self.prev_key_pressed = self.key_pressed
 		
 
 	def OpenFile(self):
@@ -152,35 +194,37 @@ class Detector():
 		self.file_object = open("/home/aakaash/fsae/src/fsae_control/data.txt", "a")
 
 	def CloseFile(self):
+		self.file_object.write("End Episode\n")
 		self.file_object.close()
 
 	def AppendToFile(self, data):
-		self.file_object.write(data + "\n")
+		self.file_object.write(data)
 
-	def GetData(self):
+	def GetCurrentData(self):
 		saved_markers = 0
 		marker_info = []
 		for i in range(self.num_of_markers):
 			if saved_markers < 6:
-				marker_pos = Point()
+				marker_pos = ArucoPositionInfo()
+				marker_pos.id = self.markers.marker_ids[i]
 				marker_pos.x = self.markers.marker_poses[i].position.x
 				marker_pos.z = self.markers.marker_poses[i].position.z
 				marker_info.append(marker_pos)
 				saved_markers += 1
 
 		while saved_markers < 6:
-			marker_pos = Point()
-			marker_pos.x = 100
-			marker_pos.z = 100
+			marker_pos = ArucoPositionInfo()
 			marker_info.append(marker_pos)
 			saved_markers += 1
 
-		return marker_info
+		self.current_marker_positions = marker_info
+		self.current_action = self.action
+		self.current_reward = self.CalculateReward()
 
 		
 
 	
-	def PrintAllMarkers(self, markerPositions):
+	def PrintAllMarkers(self):
 		print("Number of Markers = %d" % self.num_of_markers)
 		for i in range(self.num_of_markers):
 			print("Marker %d : \nX = %.3f \nY = %.3f \nZ = %.3f \n\n\n" % (self.markers.marker_ids[i],self.markers.marker_poses[i].position.x,self.markers.marker_poses[i].position.y,self.markers.marker_poses[i].position.z))
